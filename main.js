c461e23ca30b16f88d2859f841d8b01757213339
unknown <majobel10@hotmail.com>
2020-01-29 23:14:11 -0500

    Funciona agregar con elementos

--- main.js
+++ main.js
@@ -1,44 +1,36 @@
 window.addEventListener('load', function(){
-    const ul = document.querySelector('ul');
-    const input = document.querySelector('input');
-    
-    const list =[
-        'uno',
-        'dos',
-        'tres',
-    ];
-
-    list.forEach((elem)=>{
-        addNewLi(elem);
-    });
+    const cajatexto = document.querySelector(".cajatexto");
+    var lista = document.querySelector(".lista");
+    var num = document.querySelector(".numeroTotal");
+    var btnTodos = document.querySelector(".todos");
+    var btnActivos = document.querySelector(".activos");
+    var btnCompletos = document.querySelector(".completos");
+    var btnEliminar = document.querySelector(".eliminar");
 
     //AddeventListener
             // elemento que dispara el evento       INPUT
             // tipo de evento                       Keypress
             // accion                               If
-    input.addEventListener('keypress', function(event){
-        const value = input.value;
+    cajatexto.addEventListener('keypress', function AddList(event){
+        const value = cajatexto.value;
         //verifica que hay algo en el place
-        if(event.keyCode === 13 && value){
-           addNewLi(value);
-            input.value = '';
+        if(event.keyCode == 13 && value){
+           addNewLi(this.value);
+            cajatexto.value = '';
+            num.innerHTML++;
         }
     });
-function addNewLi (word){
-    const newLi = document.createElement('li');
+
+    function addNewLi (word){
+     
+    // Crear un elemento de la lista
+    const newLi = document.createElement('div');
     //agrega el texto a la memoria
-    newLi.innerText = word;
-    // Lo agrega a la lista de manera visual
-    ul.appendChild(newLi);
+    //Crea un elemento de la lista- Un input que haga el check un p para que sea el texto editable y el boton de eliminar
+    newLi.innerHTML = `<li class = "elemento"><input class="elemento__completado" type= "checkbox"><p  class="elemento__contenido" contenteditable> ${word} </p><button class="elemento__eliminar"> X </button></li> `
+    newLi.setAttribute( 'class' , 'elemento');
+    lista.appendChild(newLi);
     
-    newLi.addEventListener('click', () =>{
-        if(newLi.style.textDecoration){
-            newLi.style.textDecoration='';
-            newLi.contentEditable="true";
-        }else{
-        newLi.style.textDecoration ='line-through';
-        }
-    });
 }
   
 });
